3d1d5ec9990703a42eddcbd53c8f3d456f0ec83d
drashya03 <drashyamalot@gmail.com>
2025-12-01 19:37:45 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Login from './pages/Login'
 import Emailverify from './pages/Emailverify'
 import ResetPassword from './pages/ResetPassword'
